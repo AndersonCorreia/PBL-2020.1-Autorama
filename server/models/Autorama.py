953524cf242a61bebbb14b76d2bd61b0ec89696f
Marianasls <marianalima0803@gmail.com>
2021-03-09 18:11:48 -0300

    alteraçao nome do arquivo

---
+++ server/models/Autorama.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+import json
+import os
+class Autorama:
+    def __init__(self, file=os.path.dirname(os.path.realpath(__file__))+"/autorama.json"):
+        self.fileName = file
+        self.dados = json.loads(open(file, 'r').read() )
+        self.carros = self.dados['carros']
+    
+    def save(self):
+        open(self.fileName, 'w').write( json.dumps(self.dados, indent=4, ensure_ascii=False,))
+
+    def setLastTag(tag):
+        self.dados["tags"]["last"] = tag
+        self.save()
