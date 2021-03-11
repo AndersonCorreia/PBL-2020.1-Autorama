b2dff841451699ea8d9a3e96cbb181b9a1250372
andersoncorreia <anderson-strife@outlook.com>
2021-03-11 19:45:00 -0300

    ....

--- server/mercury_/sensor.py
+++ server/mercury_/sensor.py
@@ -10,9 +10,9 @@ autorama = Autorama()
 
 
 def read():
+    reader = mercury.Reader("tmr:///dev/ttyUSB0", baudrate=115200)
     print(reader.get_model())
     print(reader.get_supported_regions())
-    reader = mercury.Reader("tmr:///dev/ttyUSB0", baudrate=115200)
     reader.set_region("EU3")
     reader.set_read_plan([1], "GEN2", read_power=1100)
     tags = reader.read(2000)
