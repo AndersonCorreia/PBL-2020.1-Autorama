4b57ec5153d4938eabda7bcab64042314bfcf371
andersoncorreia <anderson-strife@outlook.com>
2021-03-26 20:10:11 -0300

    ....

--- server/mercury_/sensor.py
+++ server/mercury_/sensor.py
@@ -17,11 +17,11 @@ def read():
     print(reader.get_supported_regions())
     tags = reader.read(1000)
     if len(tags):
-        tag = {"EPC": {"tag": tags[0].epc.decode("utf-8") , "timestamp": tags[0].epc.decode("utf-8")} , 'success': True }
+        tag = {"dados": {"tag": tags[0].epc.decode("utf-8") , "timestamp": tags[0].epc.decode("utf-8")} , 'success': True }
         autorama.setLastTag(tag)
         return tag
     
-    return {"EPC": {"tag": '', "timestamp": ''}, 'success': False }
+    return {"dados": {"tag": '', "timestamp": ''}, 'success': False }
 
 def readAll():
     tags = reader.read()
--- server/routes.py
+++ server/routes.py
@@ -6,19 +6,19 @@ def route(data):
     data = json.loads(data)
     if data['path'] and data['method']:
         dados = redirecionamento(data['path'], data['method'], data['headers'])
-        return json.dumps({'success': dados['success'], "response": dados['EPC']})
+        return json.dumps({'success': dados['success'], "response": dados['dados']})
     else:
         return json.dumps({'success': False, "response": {"erro": "O path e/ou method não foram informados"} })
 
 def redirecionamento(path, method, headers=[]):
     if path == "/test":
         if method == "GET":# posteriormente testa uma conexão real com o leitor
-            return {"success": True}
+            return {"success": True, 'dados': ''}
 
     if path == "/config/leitor":
         if method == "POST":
             AutoramaController.setConfigLeitor(headers)
-            return {'success': True}
+            return {'success': True, 'dados': ''}
 
     if path == "/configuração/carro":
         if method == "GET":
