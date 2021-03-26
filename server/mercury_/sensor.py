30b1581964751cb4c435174442ec2e307cb9a7ba
andersoncorreia <anderson-strife@outlook.com>
2021-03-26 18:02:34 -0300

    fiz leitura da tag

--- server/mercury_/sensor.py
+++ server/mercury_/sensor.py
@@ -15,19 +15,16 @@ reader.set_read_plan([1], "GEN2", read_power=1100)
 def read():
     print(reader.get_model())
     print(reader.get_supported_regions())
-    tags = reader.read()
-    epcs = list(map(lambda tag: tag, reader.read()))
-    print(tags)
-    print(type(epcs))
+    tags = reader.read(1000)
     if len(tags):
-#       autorama.setLastTag(tags[0])
-        return epcs[0]
+        tag = {"tag": tags[0].epc, "timestamp": tags[0].epc, 'success': True }
+        autorama.setLastTag(tag)
+        return tag
     
-    return "Nenhuma tag foi lida"
+    return {"tag": '', "timestamp": '', 'success': False }
 
 def readAll():
     tags = reader.read()
-    print(tags)
     if len(tags):
         autorama.setLastTag(tags[-1])
         return tags
