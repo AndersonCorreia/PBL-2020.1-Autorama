1a7a737360139fa213d90607f8ce011d7770edb3
andersoncorreia <anderson-strife@outlook.com>
2021-04-09 16:50:06 -0300

    ...

--- exemplos/python/test.py
+++ exemplos/python/test.py
@@ -12,6 +12,6 @@ reader.set_region("EU3")
 reader.set_read_plan([1], "GEN2", read_power=1500)
 print(reader.read())
 
-reader.start_reading(lambda tag: print(tag.epc, tag.antenna, tag.read_count, tag.rssi, tag.timestamp))
+reader.start_reading(lambda tag: print(tag.epc, tag.antenna, tag.read_count, tag.rssi, tag.timestamp, datetime.fromtimestamp(tag.timestamp) ))
 time.sleep(5)
 reader.stop_reading()
