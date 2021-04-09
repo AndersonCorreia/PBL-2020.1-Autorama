0e412a8b537ae2cca606d6261b56313d875f1166
andersoncorreia <anderson-strife@outlook.com>
2021-04-09 18:15:19 -0300

    passagem do client pela função de roteamento

--- server/routes.py
+++ server/routes.py
@@ -3,15 +3,15 @@ from controllers.AutoramaController import AutoramaController
 import json
 from models import Botão
 
-def route(data):
-    data = json.loads(data, client)
+def route(data, client):
+    data = json.loads(data)
     if data['path'] and data['method']:
-        dados = redirecionamento(data['path'], data['method'], data['headers'], client)
+        dados = redirecionamento(client, data['path'], data['method'], data['headers'])
         return json.dumps({'success': dados['success'], "response": dados['dados']})
     else:
         return json.dumps({'success': False, "response": {"erro": "O path e/ou method não foram informados"} })
 
-def redirecionamento(path, method, headers=[], client):
+def redirecionamento(client, path, method, headers=[]):
     if path == "/test":
         if method == "GET":# posteriormente testa uma conexão real com o leitor
             return {"success": True, 'dados': ''}
