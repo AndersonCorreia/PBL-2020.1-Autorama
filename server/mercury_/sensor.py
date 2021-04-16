6c13894f839aa9f834295c69ba6009c9660940c5
andersoncorreia <anderson-strife@outlook.com>
2021-04-16 18:13:31 -0300

    aumentando potencia do sensor

--- server/mercury_/sensor.py
+++ server/mercury_/sensor.py
@@ -10,7 +10,7 @@ import os
 autorama = Autorama()
 reader = mercury.Reader("tmr:///dev/ttyUSB0", baudrate=115200)
 reader.set_region("NA2")
-reader.set_read_plan([1], "GEN2", read_power=1100)
+reader.set_read_plan([1], "GEN2", read_power=1500)
 
 
 def read():
