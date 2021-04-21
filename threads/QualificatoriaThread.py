d7791a47a086672aec8a6d798bf5bd8a9a7ca57f
andersoncorreia <anderson-strife@outlook.com>
2021-04-21 13:30:44 -0300

    fix nome da classe

--- threads/QualificatoriaThread.py
+++ threads/QualificatoriaThread.py
@@ -6,7 +6,7 @@ class QualificatoriaThread(Thread):
 
 	def __init__ (self, corrida_id = None):
 		Thread.__init__(self)
-		self.qualificatoria = qualificatoria(corrida_id)
+		self.qualificatoria = Qualificatoria(corrida_id)
   
 	def run(self):
 		self.qualificatoria.qualificatoria()
