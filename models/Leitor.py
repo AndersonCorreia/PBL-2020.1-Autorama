c8f7c764540b447974804dc32e8190ae9054c35e
marianasls <marianalima0803@gmail.com>
2021-05-28 00:12:23 -0300

    fix envio de dados do leitor

--- models/Leitor.py
+++ models/Leitor.py
@@ -1,5 +1,4 @@
 # coding=utf-8
-from client.src.mqtt.SUB import Subscriber
 import json
 import os
 from client.src.mqtt.PUB import Publisher
@@ -10,23 +9,22 @@ class Leitor:
         self.dados = json.loads(open(file, 'r').read() )
     
     def save(self, dados):
-        self.dados = dados
-        open(self.fileName, 'w').write( json.dumps(self.dados, indent=4, ensure_ascii=False,))
+        self.dados = dados 
+        dado_str = json.dumps(dados, indent=4, ensure_ascii=False,)
+        open(self.fileName, 'w').write( dado_str )
         connection = self.getConnection()
-        connection.request('/config/leitor', self.dados)
+        connection.request('/config/leitor', dado_str)
         return connection.requestRecv()['headers']
 
     def testConnection(self):
-        open(self.fileName, 'w').write( json.dumps(self.dados, indent=4, ensure_ascii=False,))
         connection = self.getConnection()
         connection.request('/test')
         return connection.requestRecv()['headers']
     
-    def getConnection(self, i=1):
-        if i == 1: return Publisher("node02.myqtthub.com", 1883, "2", "cliente2", "135790")
-        else: return Subscriber("node02.myqtthub.com", 1883, "2", "cliente2", "135790")
+    def getConnection(self):
+        return Publisher("node02.myqtthub.com", 1883, "2", "cliente2", "135790")
 
     def getButton(self):
-        connection = self.getConnection(2)
+        connection = self.getConnection()
         connection.request('/button')
         return connection.requestRecv()['headers']
